1fad6887fa296f54d5da4d8355d4f790d9a10718
noureddein <noureddein@gmail.com>
2021-07-02 19:47:29 +0300

    Lab 1

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        Designed By Nour Eddein
+      </footer>
+    )
+  }
+}
+
+export default Footer;
